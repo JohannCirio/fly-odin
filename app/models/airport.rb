4d0c6715ba4d5b5a287f2a87d13813db08ea3f36
Johann <johannocsantos@gmail.com>
2021-02-24 13:56:20 -0300

    Makes the association between Airport and Flight models

--- app/models/airport.rb
+++ app/models/airport.rb
@@ -1,2 +1,7 @@
 class Airport < ApplicationRecord
+  has_many :departing_flights, foreign_key: 'from_airport_id', class_name: 'Flight'
+  has_many :arriving_flights, foreign_key: 'to_airport_id', class_name: 'Flight'
+
+  validates :code, presence: true, uniqueness: true
+  validates :city, presence: true
 end
