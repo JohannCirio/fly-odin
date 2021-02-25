3f950766b8eed5349d8876f15290aab368d497df
Johann <johannocsantos@gmail.com>
2021-02-25 16:28:46 -0300

    Solved the problem where I could not access the @available_flights variable in the view.

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,4 +1,10 @@
 class FlightsController < ApplicationController
   def index
+    @airport_code_options = Airport.all.map { |airport| [airport.code, airport.id] }
+    @flights_dates = Flight.all.map.uniq { |flight| flight.departure_time }
+    @flights_dates = @flights_dates.map { |flight| [flight.formatted_date, flight.departure_time] }
+    if params.has_key?("from_airport_id")
+      @available_flights = Flight.where(from_airport_id: params[:from_airport_id],to_airport_id: params[:to_airport_id], departure_time: params[:departure_time])
+    end
   end
 end
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,14 @@
 class Flight < ApplicationRecord
   belongs_to :from_airport, class_name: 'Airport'
   belongs_to :to_airport, class_name: 'Airport'
+
+  validates :from_airport_id, presence: true
+  validates :to_airport_id, presence: true
+  validates :departure_time, presence: true
+  validates :duration, presence: true
+
+  def formatted_date
+    departure_time.strftime("%d/%m/%Y")
+  end
+    
 end
